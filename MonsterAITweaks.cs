3c584b6ede7e040b82b568d755ef72c3b14431ea
searica <143636061+searica@users.noreply.github.com>
2024-01-15 13:54:35 -0800

    Initial Commit

---
+++ MonsterAITweaks.cs
@@ -0,0 +1,166 @@
+﻿// Ignore Spelling: MonsterAITweaks Jotunn
+
+using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+using MonsterAITweaks.Configs;
+using MonsterAITweaks.Extensions;
+using System.Reflection;
+using UnityEngine;
+
+namespace MonsterAITweaks {
+
+    [HarmonyPatch]
+    [BepInPlugin(PluginGUID, PluginName, PluginVersion)]
+    [BepInDependency(Jotunn.Main.ModGuid, Jotunn.Main.Version)]
+    internal sealed class MonsterAITweaks : BaseUnityPlugin {
+        internal const string Author = "Searica";
+        public const string PluginName = "MonsterAITweaks";
+        public const string PluginGUID = $"{Author}.Valheim.{PluginName}";
+        public const string PluginVersion = "0.0.1";
+
+        // Use this class to add your own localization to the game
+        // https://valheim-modding.github.io/Jotunn/tutorials/localization.html
+        //public static CustomLocalization Localization = LocalizationManager.Instance.GetLocalization();
+
+        private static readonly string MainSection = ConfigManager.SetStringPriority("Global", 3);
+
+
+        public void Awake() {
+            Log.Init(Logger);
+
+            ConfigManager.Init(PluginGUID, Config);
+            SetUpConfigEntries();
+
+            Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly(), harmonyInstanceId: PluginGUID);
+            Game.isModded = true;
+
+            ConfigManager.SetupWatcher();
+            //ConfigManager.CheckForConfigManager();
+
+            ConfigManager.BindConfig(
+                "AAAA",
+                "AAA",
+                1f,
+                "AAA.",
+                new AcceptableValueRange<float>(0f, 1000f)
+            );
+
+        }
+
+        public void OnDestroy() {
+            ConfigManager.Save();
+        }
+
+        internal static void SetUpConfigEntries() {
+            Log.Verbosity = ConfigManager.BindConfig(
+                MainSection,
+                "Verbosity",
+                LogLevel.Low,
+                "Low will log basic information about the mod. Medium will log information that " +
+                "is useful for troubleshooting. High will log a lot of information, do not set " +
+                "it to this without good reason as it will slow Down your game.",
+                synced: false
+            );
+
+            ConfigManager.Save();
+        }
+
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(ZoneSystem), nameof(ZoneSystem.Start))]
+        private static void InitializeConfig() {
+            ConfigManager.BindConfig(
+                "AAAAA",
+                "ZoneSystemStart-Plugin",
+                1f,
+                "AAA.",
+                new AcceptableValueRange<float>(0f, 1000f)
+            );
+        }
+    }
+
+    /// <summary>
+    ///     Log level to control output to BepInEx log
+    /// </summary>
+    internal enum LogLevel {
+        Low = 0,
+        Medium = 1,
+        High = 2,
+    }
+
+    /// <summary>
+    ///     Helper class for properly logging from static contexts.
+    /// </summary>
+    internal static class Log {
+
+        internal static ConfigEntry<LogLevel> Verbosity { get; set; }
+        internal static LogLevel VerbosityLevel => Verbosity.Value;
+        internal static bool IsVerbosityLow => Verbosity.Value >= LogLevel.Low;
+        internal static bool IsVerbosityMedium => Verbosity.Value >= LogLevel.Medium;
+        internal static bool IsVerbosityHigh => Verbosity.Value >= LogLevel.High;
+
+        internal static ManualLogSource _logSource;
+
+        internal static void Init(ManualLogSource logSource) {
+            _logSource = logSource;
+        }
+
+        internal static void LogDebug(object data) => _logSource.LogDebug(data);
+
+        internal static void LogError(object data) => _logSource.LogError(data);
+
+        internal static void LogFatal(object data) => _logSource.LogFatal(data);
+
+        internal static void LogMessage(object data) => _logSource.LogMessage(data);
+
+        internal static void LogWarning(object data) => _logSource.LogWarning(data);
+
+        internal static void LogInfo(object data, LogLevel level = LogLevel.Low) {
+            if (Verbosity is null || VerbosityLevel >= level) {
+                _logSource.LogInfo(data);
+            }
+        }
+
+        internal static void LogGameObject(GameObject prefab, bool includeChildren = false) {
+            LogInfo("***** " + prefab.name + " *****");
+            foreach (Component compo in prefab.GetComponents<Component>()) {
+                LogComponent(compo);
+            }
+
+            if (!includeChildren) { return; }
+
+            LogInfo("***** " + prefab.name + " (children) *****");
+            foreach (Transform child in prefab.transform) {
+                LogInfo($" - {child.gameObject.name}");
+                foreach (Component compo in child.gameObject.GetComponents<Component>()) {
+                    LogComponent(compo);
+                }
+            }
+        }
+
+        internal static void LogComponent(Component compo) {
+            LogInfo($"--- {compo.GetType().Name}: {compo.name} ---");
+
+            PropertyInfo[] properties = compo.GetType().GetProperties(ReflectionUtils.AllBindings);
+            foreach (var property in properties) {
+                try {
+                    LogInfo($" - {property.Name} = {property.GetValue(compo)}");
+                }
+                catch {
+                    LogWarning($"Failed to get value for {property.Name}");
+                }
+            }
+
+            FieldInfo[] fields = compo.GetType().GetFields(ReflectionUtils.AllBindings);
+            foreach (var field in fields) {
+                try {
+                    LogInfo($" - {field.Name} = {field.GetValue(compo)}");
+                }
+                catch {
+                    LogWarning($"Failed to get value for {field.Name}");
+                }
+            }
+        }
+    }
+}
---
+++ MonsterDB.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using BepInEx.Configuration;
+using HarmonyLib;
+using Jotunn.Managers;
+using MonsterAITweaks.Configs;
+using MonsterAITweaks.Extensions;
+using UnityEngine;
+
+namespace MonsterAITweaks {
+
+    [HarmonyPatch]
+    internal static class MonsterDB {
+        private static readonly Dictionary<ConfigEntry<float>, GameObject> MonsterConfigMap = new();
+
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(ZoneSystem), nameof(ZoneSystem.Start))]
+        private static void InitializeDB() {
+
+            if (MonsterConfigMap.Count > 0) {
+                return;
+            }
+
+            Log.LogInfo("InitalizeMonsterDB");
+            var monsters = FindMonsters();
+            ConfigManager.SaveOnConfigSet(false);
+
+            foreach (var monster in monsters) {
+                CreateConfigs(monster);
+            }
+
+            ConfigManager.Save();
+            ConfigManager.SaveOnConfigSet(true);
+        }
+
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(Game), nameof(Game._RequestRespawn))]
+        private static void InitGame() {
+            Log.LogInfo($"PlayerIsAdmin: {SynchronizationManager.Instance.PlayerIsAdmin}");
+
+            ConfigManager.BindConfig(
+                "AAA",
+                "AAA",
+                1f,
+                "AAA.",
+                new AcceptableValueRange<float>(0f, 1000f)
+            );
+        }
+
+        private static void CreateConfigs(GameObject monster) {
+            if (!monster || !monster.TryGetComponent(out MonsterAI monsterAI)) {
+                return;
+            }
+
+            var circleDist = ConfigManager.BindConfig(
+                monster.name,
+                nameof(monsterAI.m_circleTargetDistance).RemovePrefix("m_"),
+                monsterAI.m_circleTargetDistance,
+                "Distance that monster circles target at.",
+                new AcceptableValueRange<float>(0f, 1000f)
+            );
+            MonsterConfigMap.Add(circleDist, monster);
+            monsterAI.m_circleTargetDistance = circleDist.Value;
+            circleDist.SettingChanged += UpdateCircleDistance;
+
+            var circleDuration = ConfigManager.BindConfig(
+                monster.name,
+                nameof(monsterAI.m_circleTargetDuration).RemovePrefix("m_"),
+                monsterAI.m_circleTargetDuration,
+                "Duration that monster circles target for..",
+                new AcceptableValueRange<float>(0f, 1000f)
+            );
+            MonsterConfigMap.Add(circleDuration, monster);
+            monsterAI.m_circleTargetDuration = circleDuration.Value;
+            circleDuration.SettingChanged += UpdateCircleDuration;
+
+            var circleInterval = ConfigManager.BindConfig(
+                monster.name,
+                nameof(monsterAI.m_circleTargetInterval).RemovePrefix("m_"),
+                monsterAI.m_circleTargetInterval,
+                "Interval that monster circles target at.",
+                new AcceptableValueRange<float>(0f, 1000f)
+            );
+            MonsterConfigMap.Add(circleInterval, monster);
+            monsterAI.m_circleTargetInterval = circleInterval.Value;
+            circleInterval.SettingChanged += UpdateCircleInterval;
+        }
+
+        private static bool TryGetMonsterConfig(object obj, out ConfigEntry<float> config, out GameObject monster) {
+            if (obj is ConfigEntry<float> cfg &&
+                MonsterConfigMap.TryGetValue(cfg, out monster) &&
+                monster) {
+                config = cfg;
+                return true;
+            }
+            config = null;
+            monster = null;
+            return false;
+        }
+
+        private static void UpdateCircleDistance(object obj, EventArgs args) {
+            if (TryGetMonsterConfig(obj, out ConfigEntry<float> config, out GameObject monster)) {
+                monster.GetComponent<MonsterAI>().m_circleTargetDistance = config.Value;
+                Log.LogInfo($"Set {monster.name} circle distance to {config.Value}", LogLevel.High);
+            }
+        }
+
+        private static void UpdateCircleDuration(object obj, EventArgs args) {
+            if (TryGetMonsterConfig(obj, out ConfigEntry<float> config, out GameObject monster)) {
+                monster.GetComponent<MonsterAI>().m_circleTargetDuration = config.Value;
+                Log.LogInfo($"Set {monster.name} circle duration to {config.Value}", LogLevel.High);
+            }
+        }
+
+        private static void UpdateCircleInterval(object obj, EventArgs args) {
+            if (TryGetMonsterConfig(obj, out ConfigEntry<float> config, out GameObject monster)) {
+                monster.GetComponent<MonsterAI>().m_circleTargetInterval = config.Value;
+                Log.LogInfo($"Set {monster.name} circle interval to {config.Value}", LogLevel.High);
+            }
+        }
+
+        private static List<GameObject> FindMonsters() {
+            var gameObjects = Resources.FindObjectsOfTypeAll<GameObject>(); // get all GameObjects
+            var prefabs = gameObjects.Where(x => !x.transform.parent); // get objects without a parent (ie they are root prefabs)
+            return prefabs.Where(x => x.GetComponent<MonsterAI>()).ToList(); // Get prefabs that have monsterAI component
+        }
+    }
+}
