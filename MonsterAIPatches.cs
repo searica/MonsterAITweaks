fc27e7bf0056108e9429189071b05a439e32db76
searica <143636061+searica@users.noreply.github.com>
2024-01-21 08:18:49 -0800

    Track AI state

---
+++ MonsterAIPatches.cs
@@ -0,0 +1,29 @@
+﻿//using HarmonyLib;
+
+//namespace MonsterAITweaks {
+
+//    [HarmonyPatch]
+//    internal static class MonsterAIPatches {
+
+//        [HarmonyPostfix]
+//        [HarmonyPatch(typeof(MonsterAI), nameof(MonsterAI.Awake))]
+//        private static void AwakeStatus(MonsterAI __instance) {
+//            if (!__instance) {
+//                return;
+//            }
+//            __instance.m_aiStatus = "Awake";
+//        }
+
+//        [HarmonyPostfix]
+//        [HarmonyPatch(typeof(MonsterAI), nameof(MonsterAI.UpdateAI))]
+//        private static void UpdateAIPostfix(MonsterAI __instance, float dt) {
+//            if (!__instance) {
+//                return;
+//            }
+
+//            if (__instance.name.Contains("Deathsq")) {
+//                Log.LogInfo($"AI Status: {__instance.m_aiStatus}");
+//            }
+//        }
+//    }
+//}
